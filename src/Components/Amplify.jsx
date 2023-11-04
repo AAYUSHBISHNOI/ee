7dd51b322b40d6175a29204ec0b86b7dbf67b91f
AAYUSH BISHNOI <aayushkaswan79@gmail.com>
2023-11-04 09:25:43 +0530

    header_text

--- src/Components/Amplify.jsx
+++ src/Components/Amplify.jsx
@@ -11,7 +11,7 @@ const Amplify = () => {
     <div className='position-relative mt-md-5 pt-md-5 pt-lg-0 mt-lg-0'>
     <div className='bcg_section_2'>
       <div className='text-center py-3  d-none d-md-block'>
-      <h2 className='ff_machina fs_52 fw_800 text-white ' data-aos="zoom-in-up" >Amplify and <span className='text-uppercase'>stretch gains</span></h2>
+      <h2 className=' ff_Jakarta fs_52 fw_800 text-white ' data-aos="zoom-in-up" >Amplify and <span className='text-uppercase'>stretch gains</span></h2>
       </div>
       <Container className='my_container d-flex flex-column justify-content-center min-100'>
         <p className='text-center fs_14 fw_400 text-white ff_Raleway d-none d-md-block' data-aos="zoom-in-down" >We take a different balanced approach to finding and creating yield opportunities in <span className='d-lg-block'>the everchanging crypto market. Our yield strategies help users stay protected and</span><span className='d-lg-block'> hedged during bear markets and amplify yield during bull markets without resorting</span><span className='d-lg-block'> to leverage or other dangerous techniques.</span></p>
